11ac2e9b0f2aa543677504b99d79732b55f1fd93
Amanda Rolon <abrolon87@gmail.com>
2020-09-26 23:37:49 -0400

    adds middleware to be able to accept req.body

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DB
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Manager API" }));
 
 // Define routes
