dcbdff383ca5485ae538be7efd7b9dcdab0eef43
Amanda Rolon <abrolon87@gmail.com>
2020-09-26 23:00:02 -0400

    connects Mongo database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect DB
+connectDB();
+
 app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Manager API" }));
 
+// Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
