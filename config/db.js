9d48113b6b8f42faf8175d7f0473174bc6c53b66
Amanda Rolon <abrolon87@gmail.com>
2020-09-26 23:33:38 -0400

    comment outs code that may need later

--- config/db.js
+++ config/db.js
@@ -4,11 +4,12 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(db);
+    // May need this later {
+    //   useNewUrlParser: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // };
 
     console.log("mongodb connected");
   } catch (err) {
