df70a14632f83fb18e2b42c0d1f22ea8d1797cd7
Amanda Rolon <abrolon87@gmail.com>
2020-10-02 13:01:07 -0400

    imports BrowserRouter; adds Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
+import Home from "./Components/pages/Home";
+import About from "./Components/pages/About";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      MY App
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
