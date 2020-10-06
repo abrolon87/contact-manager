1fbb34f06d6efa5a2f4140f8e865bb92e6294149
Amanda Rolon <abrolon87@gmail.com>
2020-10-05 23:30:45 -0400

    imports Register and Login and adds Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Home from "./Components/pages/Home";
 import About from "./Components/pages/About";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -19,6 +21,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
